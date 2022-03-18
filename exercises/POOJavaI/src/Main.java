68e11b3409a9aa179ca79b75c2eb1accfc7551af
itIsV <soicinivt@gmail.com>
2022-03-18 18:18:07 -0300

    static ArrayList of Hashmaps in class Pessoa

--- exercises/POOJavaI/src/Main.java
+++ exercises/POOJavaI/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
         Pessoa hommer = new Pessoa("Hommer", 39, "171", 85.00,1.78);
 
-        System.out.println(hommer);
+        Pessoa Anonymous = new Pessoa("Hom", 39, "171", 85.00,1.78);
+
+        Pessoa.pessoas.stream().forEach(System.out::println);
+
     }
+
 }
--- exercises/POOJavaI/src/Pessoa.java
+++ exercises/POOJavaI/src/Pessoa.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pessoa {
 
-    String nome;
-    int idade;
-    String ID;
-    Double peso;
-    Double altura;
+    private String nome;
+    private int idade;
+    private String ID;
+    private Double peso;
+    private Double altura;
+    public static ArrayList<HashMap> pessoas = new ArrayList();
+    private HashMap<String, String> pessoa = new HashMap();
 
     public Pessoa(String nome, int idade, String ID, Double peso, Double altura) {
 
@@ -13,6 +19,40 @@ public class Pessoa {
         this.ID = ID;
         this.peso = peso;
         this.altura = altura;
+
+        this.pessoa.put(
+            "nome", nome
+        );
+
+        this.pessoa.put(
+            "idade", Integer.toString(idade)
+        );
+
+        this.pessoa.put(
+            "ID", ID
+        );
+
+        this.pessoa.put(
+            "peso", Double.toString(peso)
+        );
+
+        this.pessoa.put(
+            "altura", Double.toString(altura)
+        );
+
+        pessoas.add(pessoa);
+
+    }
+
+    public Pessoa(String nome, int idade, String ID) {
+
+        this.nome = nome;
+        this.idade = idade;
+        this.ID = ID;
+    }
+
+    public Pessoa() {
+
     }
 
     @Override
